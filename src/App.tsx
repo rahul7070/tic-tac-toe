10b2ee9712cf0db09d06d8b44df41db16ecf286e
RAHUL RAMAN <58595285+rahul7070@users.noreply.github.com>
2023-07-23 23:56:39 +0530

    sdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Matrix from './components/Matrix'
 
--- src/components/Matrix.tsx
+++ src/components/Matrix.tsx
@@ -27,45 +27,49 @@ function Matrix() {
 
   function handleMark(e:React.MouseEvent<HTMLDivElement, MouseEvent>):void{
     const clickEvent = e.target as HTMLDivElement
+    
     if(turn<10 && box[+clickEvent.id-1]==""){
         if(turn%2==0){
             box[+clickEvent.id-1]="X";
         } else{
             box[+clickEvent.id-1]="O";
         }
-        setTurn(++turn);
         setBox(box); setFlag(true)
     }
-    let isWinner=0;
     for(let i=0; i<7; i=i+3){
         let c=0;
         for(let j=i; j<i+3; j++){
             if(box[i]!="" && box[j]==box[i])c++;
         }
-        if(c==3) return setWinner(box[+clickEvent.id-1])
+        if(c==3) return setWinner(box[+clickEvent.id-1]+" won the game")
     }
     for(let i=0; i<3; i++){
         let c=0;
         for(let j=i; j<i+7; j=j+3){
             if(box[i]!="" && box[j]==box[i])c++;
         }
-        if(c==3) return setWinner(box[+clickEvent.id-1])
+        if(c==3) return setWinner(box[+clickEvent.id-1]+" won the game")
     }
         let c=0;
         for(let j=0; j<9; j=j+4){
             if(box[0]!="" && box[j]==box[0])c++;
         }
-        if(c==3) return setWinner(box[+clickEvent.id-1])
+        if(c==3) return setWinner(box[+clickEvent.id-1]+" won the game")
         c=0;
         for(let j=2; j<8; j=j+2){
             if(box[2]!="" && box[j]==box[2])c++;
         }
-        if(c==3) return setWinner(box[+clickEvent.id-1])
+        if(c==3) return setWinner(box[+clickEvent.id-1]+" won the game")
+        if(turn==9){
+            setFlag(true)
+            return setWinner("Match Draw")
+        } 
+        setTurn(++turn);
   }
 
   return (
     <div>
-        {winner!=""?<h3 className='winnerName'>{winner} won the game</h3>:null}
+        {winner!=""?<h3 className='winnerName'>{winner}</h3>:null}
         <div className="main">
             {makeMatrix()}
         </div>
