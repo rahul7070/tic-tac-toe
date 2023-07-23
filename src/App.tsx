698348b74e850d69b8d7b596b7b898bee61ade1b
RAHUL RAMAN <58595285+rahul7070@users.noreply.github.com>
2023-07-23 23:38:29 +0530

    sd

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Matrix from './components/Matrix'
+
+function App() {
+
+  return (
+    <>
+      <h1>Tic Tac Toe Game</h1>
+      <Matrix />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Matrix.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+
+function Matrix() {
+  
+    const [box, setBox] = useState<string[]>(["","","","","","","","",""])
+    const [flag, setFlag] = useState<boolean>(false)
+    let [turn, setTurn] = useState<number>(1);
+    const [winner, setWinner] = useState<string>("")
+
+    useEffect(()=>{
+        if(winner!=""){
+            setTimeout(()=>{
+                window.location.reload();
+            }, 5000)
+        }
+        setFlag(false)
+    },[flag])
+
+  function makeMatrix():JSX.Element[] {
+    const matrixElements:JSX.Element[] = [];
+    for (let i = 1; i <= 9; i++) {
+      const color = i % 2 === 0 ? 'black' : 'white';
+      matrixElements.push(<div key={i} className={`${color} box`} id={i.toString()} onClick={handleMark}>{box[i-1]}</div>);
+    }
+    return matrixElements;
+  }
+
+  function handleMark(e:React.MouseEvent<HTMLDivElement, MouseEvent>):void{
+    const clickEvent = e.target as HTMLDivElement
+    if(turn<10 && box[+clickEvent.id-1]==""){
+        if(turn%2==0){
+            box[+clickEvent.id-1]="X";
+        } else{
+            box[+clickEvent.id-1]="O";
+        }
+        setTurn(++turn);
+        setBox(box); setFlag(true)
+    }
+    let isWinner=0;
+    for(let i=0; i<7; i=i+3){
+        let c=0;
+        for(let j=i; j<i+3; j++){
+            if(box[i]!="" && box[j]==box[i])c++;
+        }
+        if(c==3) return setWinner(box[+clickEvent.id-1])
+    }
+    for(let i=0; i<3; i++){
+        let c=0;
+        for(let j=i; j<i+7; j=j+3){
+            if(box[i]!="" && box[j]==box[i])c++;
+        }
+        if(c==3) return setWinner(box[+clickEvent.id-1])
+    }
+        let c=0;
+        for(let j=0; j<9; j=j+4){
+            if(box[0]!="" && box[j]==box[0])c++;
+        }
+        if(c==3) return setWinner(box[+clickEvent.id-1])
+        c=0;
+        for(let j=2; j<8; j=j+2){
+            if(box[2]!="" && box[j]==box[2])c++;
+        }
+        if(c==3) return setWinner(box[+clickEvent.id-1])
+  }
+
+  return (
+    <div>
+        {winner!=""?<h3 className='winnerName'>{winner} won the game</h3>:null}
+        <div className="main">
+            {makeMatrix()}
+        </div>
+    </div>
+  );
+}
+
+export default Matrix;
